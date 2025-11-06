ad06517cd4259d227cda5a1d4fd288294662550b
Lewbarr02 <lewbarrapps@gmail.com>
2025-11-06 15:05:54 -0500

    fix: enable ES modules and install morgan

--- index.js
+++ index.js
@@ -16,4 +16,7 @@ const PORT = parseInt(process.env.PORT || "3000", 10);
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`[DS] Listening on :${PORT}`);
 });
+<<<<<<< HEAD
 
+=======
+>>>>>>> 7487bde (Fix index.js syntax for Railway)
